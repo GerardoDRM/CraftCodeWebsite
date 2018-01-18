4c328c081b7623cca62e55dfded03d817e4a81ea
gerardodrm <gerardo.delarosama@udlap.mx>
2018-01-18 01:20:28 -0600

    Basic structure

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React, {Component, PropTypes} from 'react';
+import { Link } from 'react-router-dom';
+import Footer from './Footer';
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div>
+        {/* Top Img */}
+        {/* Services */}
+        {/* Projects */}
+        {/* Funny */}
+        {/* Footer */}
+        <Footer/>
+      </div>
+    )
+  }
+}
+module.exports = Home;
